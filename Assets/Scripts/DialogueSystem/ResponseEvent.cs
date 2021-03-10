9c7197343a18e2cd5b2321ffb753ac98774a4497
Pattrigue <patrick@gamehouse.dk>
2021-03-10 13:38:12 +0100

    Update ResponseEvent

--- Assets/Scripts/DialogueSystem/ResponseEvent.cs
+++ Assets/Scripts/DialogueSystem/ResponseEvent.cs
@@ -4,6 +4,7 @@ using UnityEngine.Events;
 [System.Serializable]
 public class ResponseEvent
 {
+    [SerializeField] private string name;
     [SerializeField] private DialogueObject response;
     [SerializeField] private UnityEvent onResponseSelected;
 
